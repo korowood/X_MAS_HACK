31754e591602212a97169a37845183fb52e81cc7
Dmitry <yakorovka@gmail.com>
2022-12-17 13:47:17 +0300

    up 4

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 from inference import calc_metrics
-from catboost import to_classifier
+from catboost import to_classifier, Pool
 
 TRAIN_PATH = "X_train.csv"
 MODEL_PATH = "model.pkl"
@@ -23,6 +23,7 @@ def load_data():
         # st.header('Statistics of Dataframe')
         # st.write(df.describe())
         return df, True
+
 
 # def create_df(pred, ind):
 #     ans = pd.DataFrame(data=pred, columns=['result'], index=ind)
@@ -49,12 +50,11 @@ def setup_page_preview(data: pd.DataFrame):
     )
 
 
-
 def main():
     if upload_file is not None:
         test, loaded = load_data()
 
-    # if loaded:
+        # if loaded:
 
         train = pd.read_csv(TRAIN_PATH)
         train.drop('result', axis=1, inplace=True)
@@ -66,17 +66,19 @@ def main():
 
         model = pd.read_pickle(MODEL_PATH)
         model = to_classifier(model)
-        pred = model.predict_proba(new_test)
+
+        cat_feat = ['email', 'ip', 'cardToken', 'paymentSystem', 'bankCountry', 'partyId', 'shopId', 'currency',
+                    'bin_hash', 'ms_pan_hash', 'g1', 'g2', 'g3']
+        pool = Pool(new_test, cat_features=cat_feat)
+        pred = model.predict_proba(pool)
         k = .01
         labels = [1 if x > k else 0 for x in pred[:, 1]]
 
-
         test['prev_result'] = labels
-        test['post_result'] = test.apply(lambda x: 1 if x['providerId']==ID_PR else x.prev_result, axis=1)
-        test['result'] = test['post_result'].apply(lambda x: True if x==1 else False)
+        test['post_result'] = test.apply(lambda x: 1 if x['providerId'] == ID_PR else x.prev_result, axis=1)
+        test['result'] = test['post_result'].apply(lambda x: True if x == 1 else False)
         ans = test['result']
         res = setup_page_preview(ans)
-
 
 
 if __name__ == "__main__":
